2045444b5b22d7eba224f3562681f90d76c03872
maestitia-x <maestitia0@gmail.com>
2026-04-27 21:49:23 +0300

    MongoURI validation eklendi

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
+const MONGO_URI = process.env.MONGO_URI
+
+if (!MONGO_URI) {
+    throw new Error("MONGO_URI enviroment variable is not defined")
+}
+
 const config = {
     PORT : Number(process.env.PORT || 3000),
     APP_NAME : process.env.APP_NAME || 'MyApp',
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: MONGO_URI
 }
 
 export default config
